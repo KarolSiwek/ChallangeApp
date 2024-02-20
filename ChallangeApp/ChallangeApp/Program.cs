51119e5351288c10fab979170e5eaa58b5194a21
Karol Siwek <karol1214@o2.pl>
2024-02-20 20:31:43 +0000

    dzien 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var isWoman = true;
+var age = 30;
+
+if (name == "Ewa" && age == 30 && isWoman == true)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (isWoman == true && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+
+}
+else if (isWoman == false && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+
+}
