a77aa93123a3d1afeebb2c0003ff4579cee533f2
Karol Siwek <karol1214@o2.pl>
2024-10-05 01:49:35 +0100

    praca domowa dzien 5

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,18 +1,78 @@
-﻿var name = "Ewa";
-var isWoman = true;
-var age = 30;
+﻿int number = 4594;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-if (name == "Ewa" && age == 30 && isWoman == true)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else if (isWoman == true && age < 30)
-{
-    Console.WriteLine("Kobieta ponizej 30 lat");
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-}
-else if (isWoman == false && age < 18)
+foreach (char letter in letters)
 {
-    Console.WriteLine("Niepelnoletni mezczyzna");
-
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
+Console.WriteLine("amount0");
+Console.WriteLine(counter0);
+Console.WriteLine("amount1");
+Console.WriteLine(counter1);
+Console.WriteLine("amount2");
+Console.WriteLine(counter2);
+Console.WriteLine("amount3");
+Console.WriteLine(counter3);
+Console.WriteLine("amount4");
+Console.WriteLine(counter4);
+Console.WriteLine("amount5");
+Console.WriteLine(counter5);
+Console.WriteLine("amount6");
+Console.WriteLine(counter6);
+Console.WriteLine("amount7");
+Console.WriteLine(counter7);
+Console.WriteLine("amount8");
+Console.WriteLine(counter8);
+Console.WriteLine("amount9");
+Console.WriteLine(counter9);
